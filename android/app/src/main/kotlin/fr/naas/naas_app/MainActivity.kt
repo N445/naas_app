92108faeb2c29d23e0bf5b0d9c17a862c6a80169
Robin <parisrob@hotmail.fr>
2019-12-28 20:31:14 +0100

    initial commit tuto

---
+++ android/app/src/main/kotlin/fr/naas/naas_app/MainActivity.kt
@@ -0,0 +1,12 @@
+package fr.naas.naas_app
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
